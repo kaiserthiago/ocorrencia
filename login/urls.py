ec3c0ce0dd98029aa84de5ed6fd873cea78ed605
kaiserthiago <thiagokaiser@outlook.com>
2018-03-17 12:26:50 -0400

    dashboard

--- login/urls.py
+++ login/urls.py
@@ -1,11 +1,11 @@
+from django.conf.urls import url
 from django.contrib.auth import views as auth_views
-from django.urls import path
 
 from . import views
 
 urlpatterns = [
-    path('register/', views.register, name='login_register'),
-    path('register/success', views.register_success, name='login_register_success'),
-    path('login/', auth_views.login, name='login'),
-    path('logout/', auth_views.logout, {'next_page': '/'}, name='logout'),
+    url(r'^register$', views.register, name='login_register'),
+    url(r'^register/success$', views.register_success, name='login_register_success'),
+    url(r'^login$', auth_views.login, name='login'),
+    url(r'^logout$', auth_views.logout, {'next_page': '/'}, name='logout'),
 ]
