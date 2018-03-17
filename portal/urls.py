0be9e40ae3236cb82621a884135f57efa0ad7ac9
kaiserthiago <thiagokaiser@outlook.com>
2018-03-17 12:23:47 -0400

    dashboard

--- portal/urls.py
+++ portal/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url('^', views.home, name='home'),
+    url('^$', views.home, name='home'),
 
     url('^contato$', views.contato, name='contato'),
 
