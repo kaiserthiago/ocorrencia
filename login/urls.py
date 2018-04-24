0e8211e64c2fb40bee2de6840b989cee0ac89c12
kaiserthiago <thiagokaiser@outlook.com>
2018-04-24 15:55:10 -0400

    forgot password

--- login/urls.py
+++ login/urls.py
@@ -10,6 +10,12 @@ usuario = Empresa.objects.all().order_by('nome_fantasia')
 urlpatterns = [
     url(r'^register$', views.register, name='login_register'),
     url(r'^register/success$', views.register_success, name='login_register_success'),
-    url(r'^login$', auth_views.login, {'extra_context':{'unidades': usuario}}, name='login'),
+    url(r'^login$', auth_views.login, {'extra_context': {'unidades': usuario}}, name='login'),
     url(r'^logout$', auth_views.logout, {'next_page': '/'}, name='logout'),
+
+    url(r'^password_reset/$', auth_views.password_reset, name='password_reset'),
+    url(r'^password_reset/done/$', auth_views.password_reset_done, name='password_reset_done'),
+    url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+        auth_views.password_reset_confirm, name='password_reset_confirm'),
+    url(r'^reset/done/$', auth_views.password_reset_complete, name='password_reset_complete'),
 ]
