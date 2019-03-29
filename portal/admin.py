032b3e2a1cf43d39948f527040307c6834ea011e
kaiserthiago <thiagokaiser@outlook.com>
2019-03-28 21:27:07 -0400

    alterar visualização no admin dos encaminhamentos

--- portal/admin.py
+++ portal/admin.py
@@ -51,7 +51,7 @@ class OcorrenciaAdmin(admin.ModelAdmin):
 
 class EncaminhamentoAdmin(admin.ModelAdmin):
     list_display = (
-        'id', 'created_at', 'update_at', 'matricula', 'data', 'descricao', 'providencias', 'servico', 'user')
+        'id', 'created_at', 'update_at', 'matricula', 'descricao', 'status')
     list_filter = ['status', 'servico__categoria', 'matricula__ano_letivo', 'empresa']
     search_fields = ['matricula__aluno__nome', 'user']
 
